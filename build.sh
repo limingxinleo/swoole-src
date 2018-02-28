1e196d71100ef0e6c54f5a6b5e47f60e4f4b4d88
李铭昕 <715557344@qq.com>
2018-02-28 15:47:58 +0800

    增加传参

--- build.sh
+++ build.sh
@@ -1,3 +1,27 @@
+ARCH=
+PHPIZE_BIN=$(command -v phpize 2>/dev/null)
+PHPCONFIG_BIN=$(command -v php-config 2>/dev/null)
+
+# Translate long options to short
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--arch") set -- "$@" "-a" ;;
+    "--phpize") set -- "$@" "-i" ;;
+    "--php-config") set -- "$@" "-c" ;;
+    *) set -- "$@" "$arg"
+  esac
+done
+
+# Options switcher
+while getopts a:i:c: opts; do
+   case ${opts} in
+      a) ARCH=${OPTARG} ;;
+      i) PHPIZE_BIN=${OPTARG} ;;
+      c) PHPCONFIG_BIN=${OPTARG} ;;
+   esac
+done
+
 git submodule update --init --recursive
 cd thirdparty/hiredis
 make 
@@ -8,8 +32,8 @@ make
 sudo make install
 sudo ldconfig
 cd ../..
-phpize --clean
-phpize
+$PHPIZE_BIN --clean
+$PHPIZE_BIN
 make clean
 ./configure --enable-openssl --enable-sockets --enable-async-redis --enable-mysqlnd --enable-http2
 make -j
